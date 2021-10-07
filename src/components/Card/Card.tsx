1156a349d625824040b5fa5a716404c7ac3b64b5
Dzmitry Ihnatovich <ignatovich.dm@gmail.com>
2021-10-07 09:14:43 +0200

    Style changes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,28 +5,36 @@ import Button from "../Button/Button"
 import styles from "./styles.module.css"
 
 interface CardProps {
-    title: string
+    name: string
     price: string
+    shortDescription: string
 }
 
-const Card: React.FC<CardProps> = ({title, price}) => {
+const Card: React.FC<CardProps> = ({name, price, shortDescription}) => {
     return (
         <div className={styles.card}>
+            <div>
             <Image
                 alt="Dishes"
                 src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3570&q=80"
                 width={1302}
                 height={868}
                 layout="responsive"
+                loading="eager" //check this property in future
             />
-            <div className={styles.content}>
-                <p className={styles.cardTitle}>{title}</p>
-                <p>Text with some information</p>
-                <p>{price}</p>
+                <div className={styles.content}>
+                    <p className={styles.cardTitle}>{name}</p>
+                    <p className={styles.desc}>
+                        {shortDescription}
+                    </p>
+                </div>                
+            </div>
+            
+            <div className={styles.footer}>
+                <p className={styles.price}>{price}</p>
                 <div className={styles.btnContainer}>
                     <Button title="Add to order" onClick={() => {}}/>
                 </div>
-                
             </div>
         </div>
     )
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,15 +1,19 @@
-import React from "react"
+import React, { useMemo } from "react"
 import { Card } from "../components"
 import styles from "../../styles/Menu.module.css"
 
+const arr = ["Soup", "Meat", "Chicken", "Something else", "New"]
+
 const Menu: React.FC = () => {
+    const menuCards = useMemo(() => {
+        return arr.map((item, index) => <Card key={index} name={item} price="5.50" shortDescription="Shorttt sd fs fsd fsd fs fs fs fs df dfs sdfsdfsdf"/>)
+    }, [arr])
+
     return (
         <>
             <h1>Menu</h1>
             <div className={styles.container}>
-                <Card title="Soup" price="3.00"/>
-                <Card title="Meat" price="5.50"/>
-                <Card title="Chicken"price="10.00"/>
+                {menuCards}
             </div>
         </>
     )
