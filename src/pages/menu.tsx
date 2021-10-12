c219c4305d3a6c86ce992b6a52f0c1faec239034
Dzmitry Ihnatovich <ignatovich.dm@gmail.com>
2021-10-12 23:54:31 +0200

    Added categories

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,18 +1,7 @@
 import React, { useMemo } from 'react'
 import { Card } from '../components'
 import styles from '../../styles/Menu.module.css'
-
-const arr = [
-  {
-    uid: 1,
-    name: 'Soup with frikadels cand mushrooms very tasty',
-    priceCurrency: 'USD',
-  },
-  { uid: 2, name: 'Meat', priceCurrency: 'EURO' },
-  { uid: 3, name: 'Chicken', priceCurrency: 'POUND' },
-  { uid: 4, name: 'Something else', priceCurrency: 'ROUBLE' },
-  { uid: 5, name: 'New' },
-]
+import menu from '../../__fixtures__/menu.json'
 
 const openDetails = (id: string | number) => {
   console.log(id)
@@ -22,20 +11,31 @@ const addToBasket = (id: string | number) => {
   console.log('added', id)
 }
 
+const renderCards = (items: Array<Record<string, any>>) => {
+  return items.map(({ uid, name, priceCurrency }) => (
+    <Card
+      key={uid}
+      name={name}
+      price="5.50"
+      priceCurrency={priceCurrency}
+      onCardClick={() => openDetails(uid)}
+      shortDescription="Shorttt sd fs fsd fsd fs fs fs fs df dfs sdfsdfsdf sdf s"
+      addToBasket={() => addToBasket(uid)}
+    />
+  ))
+}
+
 const Menu: React.FC = () => {
-  const menuCards = useMemo(() => {
-    return arr.map(({ uid, name, priceCurrency }) => (
-      <Card
-        key={uid}
-        name={name}
-        price="5.50"
-        priceCurrency={priceCurrency}
-        onCardClick={() => openDetails(uid)}
-        shortDescription="Shorttt sd fs fsd fsd fs fs fs fs df dfs sdfsdfsdf sdf s"
-        addToBasket={() => addToBasket(uid)}
-      />
-    ))
-  }, [arr])
+  const menuCards = menu.map((menuItem) => {
+    return (
+      <div key={menuItem.category} className={styles.categoryContainer}>
+        <h1 className={styles.categoryTitle}>{menuItem.category}</h1>
+        <div className={styles.cardsContainer}>
+          {renderCards(menuItem.items)}
+        </div>
+      </div>
+    )
+  })
 
   return <div className={styles.container}>{menuCards}</div>
 }
